6b6ee7c0b28cadcca91907b75c6016ec03d7e052
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2025-04-10 14:52:16 +0500

    restaurants page, some routing errors fixed, profile page initiated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,13 +11,19 @@ router.put("/profile/update", protectRoute, userController.updateProfile);
 router.put("/profile/allergies", protectRoute, userController.updateAllergies);
 
 router.get("/:username", protectRoute, authorizeRoles("super-admin"), userController.getUserByUsername);
-router.put("/update-user/:username",  protectRoute, authorizeRoles("super-admin"), userController.updateUserByUsername);
+router.put("/update-user/:username", protectRoute, authorizeRoles("super-admin"), userController.updateUserByUsername);
 
 router.get("/:userId", protectRoute, authorizeRoles("admin", "manager", "super-admin"), userController.getUserById);
 router.get(
   "/:username",
   protectRoute,
   authorizeRoles("admin", "manager", "super-admin"),
+  userController.getUserByUsername
+);
+router.get(
+  "/:userId",
+  protectRoute,
+  authorizeRoles("admin", "manager", "user", "super-admin"),
   userController.getUserByUsername
 );
 router.put(
